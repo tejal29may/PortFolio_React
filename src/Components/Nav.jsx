e59952fa034e55f023fd6b42d1d468d99f51f7a7
tejal29may <85352094+tejal29may@users.noreply.github.com>
2024-03-18 20:38:32 +0530

    ..

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./Nav.css";
 import { Outlet, Link, NavLink } from "react-router-dom";
 import 'animate.css';
-
-function Nav() {
+import ToggleColorMode from "./Theme";
+function Nav({theme,setTheme}) {
   return (
     <>
       <div className="nav animate__animated animate__fadeInDown  animate__slow">
@@ -17,7 +17,9 @@ function Nav() {
             <li><NavLink to="/About" style={{textDecoration:"none"}}>About</NavLink></li>
             <li><NavLink to="/Projects" style={{textDecoration:"none"}}>Projects</NavLink></li>
             <li><NavLink to="/Contact" style={{textDecoration:"none"}}>Contact </NavLink></li>
-            <li>Mode</li>
+            <li 
+          ><a href="https://drive.google.com/file/d/1T735AVuipt2CEoUMfcnh_9IYrCgYBYcS/view?usp=sharing" style={{textDecoration:"none"}}>Resume</a></li>
+            {/* <li><ToggleColorMode/></li> */}
             
             {/* <li><button style={{backgroundColor:"pink", color:"black", padding:"3px 15px", border:"none", borderRadius:"5px", fontSize:"20px"}}>mode</button></li> */}
             
--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../Screens/Home.css"
 import Nav from '../Components/Nav'
 import Home_Section1 from '../Components/Home_Section1'
 import Home_section2 from '../Components/Home_section2'
 function Home() {
+  const[theme,setTheme]=useState(true)
   return (
-    <div className='home'>
-<Nav/>
+    
+    <div className='home' style={{backgroundColor:theme?"rgb(29, 29, 29)":"white"}}>
+<Nav theme={theme} setTheme={setTheme}/>
 <Home_Section1/>
 <Home_section2/>
     </div>
