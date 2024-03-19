54fba2fda49e4c5d2e9c2eb72bfad028009d2f3c
tejal29may <85352094+tejal29may@users.noreply.github.com>
2024-03-19 14:09:34 +0530

    ..

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,31 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.css";
 import { Outlet, Link, NavLink } from "react-router-dom";
-import 'animate.css';
+import "animate.css";
+import { MdMenu } from "react-icons/md";
+import { AiOutlineMenu } from "react-icons/ai";
 import ToggleColorMode from "./Theme";
-function Nav({theme,setTheme}) {
+function Nav({ theme, setTheme }) {
+  const [displayd, setDisplay] = useState(false);
   return (
     <>
       <div className="nav animate__animated animate__fadeInDown  animate__slow">
         <div className="left">
-          
-          <NavLink to="/" style={{textDecoration:"none"}}><p>Tejal Mohod</p></NavLink>
+          <NavLink to="/" style={{ textDecoration: "none" }}>
+            <p>Tejal Mohod</p>
+          </NavLink>
+        </div>
+        <div className="menu" onClick={()=>{
+          setDisplay(!displayd)
+        }}>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
         <div className="right">
-          <ul>
-            <li><NavLink to="/" style={{textDecoration:"none"}}>Home</NavLink></li>
-            <li><NavLink to="/About" style={{textDecoration:"none"}}>About</NavLink></li>
-            <li><NavLink to="/Projects" style={{textDecoration:"none"}}>Projects</NavLink></li>
-            <li><NavLink to="/Contact" style={{textDecoration:"none"}}>Contact </NavLink></li>
-            <li 
-          ><a href="https://drive.google.com/file/d/1T735AVuipt2CEoUMfcnh_9IYrCgYBYcS/view?usp=sharing" style={{textDecoration:"none"}}>Resume</a></li>
-            {/* <li><ToggleColorMode/></li> */}
-            
-            {/* <li><button style={{backgroundColor:"pink", color:"black", padding:"3px 15px", border:"none", borderRadius:"5px", fontSize:"20px"}}>mode</button></li> */}
-            
+          <ul className={displayd?"open":""}>
+            <li>
+              <NavLink to="/" style={{ textDecoration: "none" }}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/About" style={{ textDecoration: "none" }}>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Projects" style={{ textDecoration: "none" }}>
+                Projects
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Contact" style={{ textDecoration: "none" }}>
+                Contact{" "}
+              </NavLink>
+            </li>
+            <li>
+              <a
+                href="https://drive.google.com/file/d/1T735AVuipt2CEoUMfcnh_9IYrCgYBYcS/view?usp=sharing"
+                style={{ textDecoration: "none" }}
+              >
+                Resume
+              </a>
+            </li>
           </ul>
         </div>
-        
       </div>
     </>
   );
